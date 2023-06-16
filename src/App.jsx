100feeb1e3d1b9fb0953b5ad86862790c191f2c8
Zarima Moon <zarima1991@yahoo.com>
2023-06-16 04:27:52 -0700

    added some changes to puppy pals

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
+import { useState } from "react";
+import { puppyList } from "./data.js";
+import "./App.css";
 
-import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick(id) {
+    setFeatPupId(id);
+    console.log(featPupId);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+    console.log(featuredPup);
+  
 
   return (
-    <>
-      <div>
-      
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    
+      <div className="App">
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>  
+    )}
+        {puppies.map((puppy) => {
+          return (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+})}
+  </div>        
+  );
+        }
+    
+export default App;
